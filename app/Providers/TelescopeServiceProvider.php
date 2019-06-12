50b034a61b878c934c3989638713186cb5fc3c94
nfangxu <nfangxu@gmail.com>
2019-06-12 22:31:14 +0800

    fixed code

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,14 +21,14 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->isLocal()) {
+            // if ($this->app->isLocal()) {
                 return true;
-            }
+            // }
 
-            return $entry->isReportableException() ||
-                   $entry->isFailedJob() ||
-                   $entry->isScheduledTask() ||
-                   $entry->hasMonitoredTag();
+            // return $entry->isReportableException() ||
+            //        $entry->isFailedJob() ||
+            //        $entry->isScheduledTask() ||
+            //        $entry->hasMonitoredTag();
         });
     }
 
